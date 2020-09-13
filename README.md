b97d877088836b67ffbe0eeaf5f57b9785d099a9
jibinkoshy <jibinkoshy@hotmail.com>
2020-09-13 12:34:01 -0500

    connect to MongoDB

---
+++ README.md
@@ -0,0 +1,5 @@
+Tech stack
+
+NodeJS
+ExpressJS
+MongoDB
--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 // importing
-import express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
+const { connection_url } = require('./config');
+const { Messages } = require('./dbMessages');
 
 // app config
 const app = express();
+const port = process.env.PORT || 9000;
 
 // middleware
+app.use(express.json());
 
 // DB config
+
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // api routes
 app.get('/', (req, res) => {
   res.status(200).send('hello world');
 });
 
-// listeners
+app.post('/api/v1/messages/new', (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created : \n ${data}`);
+    }
+  });
+});
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
